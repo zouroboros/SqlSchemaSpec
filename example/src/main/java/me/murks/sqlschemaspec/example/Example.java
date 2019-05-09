c0f425d5b175f7929981541c2ec4adc933e10c0b
Zouroboros <zouroboros@zoho.eu>
2019-05-09 09:35:20 +0200

    Improved documentation.

--- example/src/main/java/me/murks/sqlschemaspec/example/Example.java
+++ example/src/main/java/me/murks/sqlschemaspec/example/Example.java
@@ -1,10 +1,16 @@
 package me.murks.sqlschemaspec.example;
 
+/**
+ * Class that uses the example @{@link Schema}
+ */
 public class Example {
-	public static void main(String[] args) {
-		Schema schema = new Schema();
-		for (String statement: schema.createStatement()) {
-			System.out.println(statement);
-		}
-	}
+    public static void main(String[] args) {
+        // create the schema
+        Schema schema = new Schema();
+
+        // the sql statements to create the schema
+        for (String statement: schema.createStatement()) {
+            System.out.println(statement);
+        }
+    }
 }
--- example/src/main/java/me/murks/sqlschemaspec/example/Schema.java
+++ example/src/main/java/me/murks/sqlschemaspec/example/Schema.java
@@ -6,16 +6,48 @@ import me.murks.sqlschemaspec.TableSpec;
 import me.murks.sqlschemaspec.Type;
 import me.murks.sqlschemaspec.templates.TemplateCompiler;
 
+/**
+ * Class that defines the example schema.
+ */
 public class Schema extends SchemaSpec {
+
+    /**
+     * Inner class that defines a table.
+     */
     class Recipes extends TableSpec {
         ColumnSpec id = primaryKey(Type.Integer);
         ColumnSpec name = column(Type.String);
     }
 
+    /**
+     * Creating a field with a type that extends @{@link TableSpec} adds a table to the schema.
+     */
     Recipes recipes = new Recipes();
 
+    /**
+     * Definition of another table.
+     */
+    class Ingredients extends TableSpec {
+        ColumnSpec id = primaryKey(Type.Integer);
+        ColumnSpec recipeId = foreignKey(recipes.id);
+        ColumnSpec name = column(Type.String);
+        ColumnSpec mass = column(Type.Float, true);
+    }
+
+    /**
+     * Adding the ingredients table to the schema.
+     */
+    Ingredients ingredients = new Ingredients();
+
+    /**
+     * Constructor that invokes the @{@link TemplateCompiler#compileTable(Object, TableSpec)} method in order to compile
+     * the schema. Compiling a @{@link SchemaSpec} sets all relevant fields to create a usable schema specification.
+     */
     public Schema() {
+        // creates a new template compiler instance
         TemplateCompiler compiler = new TemplateCompiler();
+
+        // compiles the schema
         compiler.compileTemplate(this, this);
     }
 }
